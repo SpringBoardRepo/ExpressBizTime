f66cc3b9ac89912d03d4eb49723f771a93cc023c
poorvarathore05 <poorvarathore05@gmail.com>
2021-08-18 16:13:08 -0700

    create db

--- db.js
+++ db.js
@@ -1,2 +1,20 @@
 /** Database setup for BizTime. */
 
+const { Client } = require('pg')
+
+let DB_URI;
+
+if (process.env.NODE_ENV === 'test') {
+    DB_URI = "postgres://postgres:postgresql@localhost:5432/biztime_test"
+}
+else {
+    DB_URI = "postgres://postgres:postgresql@localhost:5432/biztime";
+}
+
+let db = new Client({
+    connectionString: DB_URI
+}
+)
+db.connect();
+
+module.exports = db;
