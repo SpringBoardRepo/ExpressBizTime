6bd573631565ea75f80a8c8fc8fae21acbeaf7fd
poorvarathore05 <poorvarathore05@gmail.com>
2021-08-18 18:30:55 -0700

    added invoice details in route

--- routes/companies.js
+++ routes/companies.js
@@ -16,11 +16,14 @@ router.get('/:code', async (req, res, next) => {
     try {
         const { code } = req.params;
         const result = await db.query(`SELECT * FROM companies WHERE code = $1`, [code]);
-
+        const invoicesRes = await db.query(`SELECT * from invoices WHERE comp_code =$1`, [code]);
         if (result.rows.length === 0) {
             throw new ExpressError(`Company code ${code} Not Found`);
         }
-        return res.json({ company: result.rows[0] });
+        const company = result.rows[0];
+        const invoices = invoicesRes.rows;
+        company.invoicesRes = invoices.map(inv => inv.id)
+        return res.json({ company: company });
     } catch (error) {
         next(error)
     }
