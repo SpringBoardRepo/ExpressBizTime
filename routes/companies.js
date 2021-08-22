ad43ce69676742ca7b6d2b7d74c81cdfbd9f1300
poorvarathore05 <poorvarathore05@gmail.com>
2021-08-21 17:43:42 -0700

    for insdusty

--- routes/companies.js
+++ routes/companies.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 const ExpressError = require('../expressError');
+const slugify = require('slugify');
 
 
 router.get('/', async function (req, res, next) {
@@ -17,12 +18,18 @@ router.get('/:code', async (req, res, next) => {
         const { code } = req.params;
         const result = await db.query(`SELECT * FROM companies WHERE code = $1`, [code]);
         const invoicesRes = await db.query(`SELECT * from invoices WHERE comp_code =$1`, [code]);
+
+        const industriesRes = await db.query(`SELECT c.code,c.name,c.description,i.name FROM companies AS c
+        JOIN industires_company ON comp_code=c.code
+        JOIN industries AS i ON ins_code = i.industry_code WHERE comp_code=$1`, [code]);
         if (result.rows.length === 0) {
             throw new ExpressError(`Company code ${code} Not Found`, 404);
         }
         const company = result.rows[0];
         const invoices = invoicesRes.rows;
+        const industries = industriesRes.rows;
         company.invoicesRes = invoices.map(inv => inv.id)
+        company.industries = industries.map(ins => ins.code)
         return res.json({ company: company });
     } catch (error) {
         next(error)
@@ -33,7 +40,9 @@ router.get('/:code', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
 
     try {
-        const { code, name, description } = req.body;
+
+        const { name, description } = req.body;
+        const code = slugify(name, { lower: true });
         const result = await db.query(`INSERT INTO companies (code,name,description) VALUES ($1,$2,$3) 
             RETURNING * `, [code, name, description]);
 
@@ -46,7 +55,7 @@ router.post('/', async (req, res, next) => {
 
 router.patch('/:code', async (req, res, next) => {
     try {
-        const { code } = req.params;
+        const code = req.params.code;
         const { name, description } = req.body;
 
         const result = await db.query(`UPDATE companies SET name=$1,description=$2 WHERE code =$3 RETURNING  * `, [name, description, code])
--- routes/invoices.js
+++ routes/invoices.js
@@ -46,8 +46,27 @@ router.patch('/:id', async (req, res, next) => {
 
     try {
         const id = req.params.id;
-        const { amt } = req.body;
-        const result = await db.query(`UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING *`, [amt, id]);
+        const { amt, paid } = req.body;
+        let paidDate = null;
+
+        const currResult = await db.query(`SELECT paid FROM invoices WHERE id=$1`, [id]);
+
+        if (currResult.rows.length === 0) {
+            throw new ExpressError(`No such invoice: ${id}`, 404);
+        }
+
+        const currPaidDate = currResult.rows[0].paid_date;
+        if (!currPaidDate && paid) {
+            paidDate = new Date();
+        }
+        else if (!paid) {
+            paidDate = null;
+        }
+        else {
+            paidDate = currPaidResult;
+        }
+        const result = await db.query(`UPDATE invoices SET amt=$1 paid=$2 paid_date=$3 WHERE id=$4 
+        RETURNING *`, [amt, paid, paidDate, id]);
         if (result.rows.length === 0) {
             throw new ExpressError(`Invoice id ${id} Not Found`, 404);
         }
